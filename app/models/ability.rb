6f0a985abe886e726d2d56d2ac02221ab6bc9458
barthez <bartek.bulat@gmail.com>
2013-03-26 08:15:06 +0000

    Fix user abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,32 +2,33 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new
 
-    can :manage, :all if user.admin?
+    unless user.blank?
+      can :manage, :all if user.admin?
 
-    can :create, Item do |item|
-      user.admin? || (item.folder.user_id == user.id && item.user_id == user.id)
+      can :create, Item do |item|
+        user.admin? || (item.folder.user_id == user.id && item.user_id == user.id)
+      end
+
+      can [:read, :update, :destroy], Item do |item|
+        user.admin? || item.user_id == user.id
+      end 
+
+
+      can :create, Folder do |f|
+        (f.global? && user.admin?) || (! f.global? && f.user_id == user.id)
+      end
+
+      can :destroy, Folder do |f|
+        user.admin? || f.user_id == user.id
+      end
+
+      can :create, TransferFile
+      can :destroy, TransferFile, user_id: user.id
+
+      can [:read, :create], Transfer
+      can [:update, :destroy], Transfer, user_id: user.id
     end
-
-    can [:read, :update, :destroy], Item do |item|
-      user.admin? || item.user_id == user.id
-    end unless user.new_record?
-
-
-    can :create, Folder do |f|
-      (f.global? && user.admin?) || (! f.global? && f.user_id == user.id)
-    end
-
-    can :destroy, Folder do |f|
-      user.admin? || f.user_id == user.id
-    end
-
-    can :create, TransferFile unless user.new_record?
-    can :destroy, TransferFile, user_id: user.id
-
-    can [:read, :create], Transfer unless user.new_record?
-    can [:update, :destroy], Transfer, user_id: user.id
 
   end
 end
