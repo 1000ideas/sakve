851eec9db6108397a72c7893fe2e8ac8b11419e1
barthez <bartek.bulat@gmail.com>
2014-01-13 13:30:31 +0100

    fix layout. default locale to pl

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module Sakve
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', 'admin', '*.{rb,yml}').to_s]
     config.i18n.available_locales = [:pl, :en]
+    config.i18n.default_locale = :pl
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
