d384fd9c95d4abb05b257fbe4e79590de599e2c5
kkosman <krzysztof@1000i.pl>
2014-01-16 17:21:11 +0100

    precompiles

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ config.action_mailer.perform_deliveries = true;
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( login.js login.css )
+  config.assets.precompile += %w( login.js login.css html5shiv.js uploadify.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
