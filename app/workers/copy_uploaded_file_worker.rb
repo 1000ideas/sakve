d6ceee83fd121a6d5cd78db32f333921ee6695ee
barthez <bartek.bulat@gmail.com>
2014-08-26 15:30:35 +0200

    Better Copy Worker

--- app/workers/copy_uploaded_file_worker.rb
+++ app/workers/copy_uploaded_file_worker.rb
@@ -3,12 +3,12 @@ class CopyUploadedFileWorker
 
   def perform(id, path)
     @transfer_file = TransferFile.find(id)
-    if File.exists?(path)
-      @transfer_file.update_attributes(object: File.open(path, 'rb'), upload_status: :done)
-      File.unlink(path) if File.exists?(path)
-    else
-      @transfer_file.update_attributes(upload_status: :fail)
-    end
+    file = File.open(path, 'rb')
+    @transfer_file.update_attributes(object: file, upload_status: :done)
+    File.unlink(path) if File.exists?(path)
+  rescue Errno::ENOENT
+    logger.error "File '#{path}' doesn't exists. Mark upload as failed!"
+    @transfer_file.update_attributes(upload_status: :fail) if @transfer_file
   rescue ActiveRecord::RecordNotFound
     logger.error "TransferFile##{id} doesn't exists. Skip upload."
   end
