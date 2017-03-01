6d8f56c6021c3f56568fde8ab4459d05d8b6d69e
Honorata <admin@1000i.pl>
2017-03-01 09:54:29 +0100

    Server changes

--- config/initializers/sakve.rb
+++ config/initializers/sakve.rb
@@ -2,10 +2,11 @@ require 'sakve'
 
 Sidekiq.configure_server do |config|
   ActiveRecord::Base.logger = Sidekiq.logger
+  config.redis = { url: 'unix:/var/run/redis/redis.sock', namespace: 'sakve' }
 end
 
 Sidekiq.configure_client do |config|
-
+  config.redis = { url: 'unix:/var/run/redis/redis.sock', namespace: 'sakve' }
 end
 
 class Dir
