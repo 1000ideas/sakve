a1563eb0649cff2256905c66f4f1bf8f9748d352
sbulat <szczepan.bulat@gmail.com>
2017-09-13 10:24:39 +0200

    fix redis config

--- config/initializers/sakve.rb
+++ config/initializers/sakve.rb
@@ -4,11 +4,11 @@ sidekiq_config = { url: ENV['JOB_WORKER_URL'] || 'redis://127.0.0.1:6379/0' }
 
 Sidekiq.configure_server do |config|
   ActiveRecord::Base.logger = Sidekiq.logger
-  config.redis = { url: 'unix:/var/run/redis/redis.sock', namespace: 'sakve' }
+  config.redis = { url: ENV['JOB_WORKER_URL'] || 'unix:/var/run/redis/redis.sock', namespace: 'sakve' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'unix:/var/run/redis/redis.sock', namespace: 'sakve' }
+  config.redis = { url: ENV['JOB_WORKER_URL'] || 'unix:/var/run/redis/redis.sock', namespace: 'sakve' }
 end
 
 class Dir
